07fbde9cf2fcd21eed619be3b53cd5502bb2c232
souad988 <59707859+souad988@users.noreply.github.com>
2022-05-16 14:20:20 +0100

    remove get rental data from user instruction to utils module

--- rentals_controller.rb
+++ rentals_controller.rb
@@ -1,32 +1,9 @@
-require_relative './rental'
-
 class RentalsController
   def initialize
     @rentals = []
   end
 
-  def add(people, books)
-    puts 'chose index of the book from this list'
-    available_books = books.list
-    if available_books.zero?
-      puts 'Please add some books'
-      return
-    end
-
-    book_index = gets.chomp
-    book = books.books[book_index.to_i - 1]
-    puts 'chose index of the person from this list'
-    people = people.list
-    if people.zero?
-      puts 'Please add some people'
-      return
-    end
-    print 'Person:'
-    person_index = gets.chomp
-    person = people.people[person_index.to_i - 1]
-    print 'Date:'
-    date = gets.chomp
-
+  def add(date, book, person)
     @rentals.push(Rental.new(date, book, person))
     puts 'rental added successfully!'
   end
--- utils.rb
+++ utils.rb
@@ -7,4 +7,15 @@ module Utils
     end
     data
   end
+
+  def self.list_data(object_controler)
+    list_length = object_controler.list
+    if list_length.zero?
+      puts 'Please add new items '
+      return -1
+    end
+    print 'select index from the list :'
+    index = gets.chomp
+    index.to_i
+  end
 end
