e63497a4cda1dca548f59bda19f4f479c8263a52
Bavon101 <bavonfate@gmail.com>
2022-05-17 14:02:03 +0300

    Add serializers

--- teacher.rb
+++ teacher.rb
@@ -3,12 +3,26 @@ require('./person')
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(specialization, age, name = 'Unknown')
-    super(age, name, parent_permission: true)
+  def initialize(id, specialization, age, name = 'Unknown')
+    super(age, name, id, parent_permission: true)
     @specialization = specialization
   end
 
   def can_use_services?
     true
+  end
+
+  def self.from_json(json)
+    Teacher.new(json['id'], json['specialization'], json['age'], json['name'])
+  end
+
+  def self.to_json
+    obj = {
+      'id' => @id,
+      'age' => @age,
+      'name' => @name,
+      'specialization' => @specialization
+    }
+    JSON.generate(obj)
   end
 end
