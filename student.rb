584f3b1a4e408f0212409f845332354b73885c79
Bavon101 <bavonfate@gmail.com>
2022-05-17 14:01:49 +0300

    Add serializers

--- student.rb
+++ student.rb
@@ -2,8 +2,8 @@ require('./person')
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    super(age, name, id, parent_permission: parent_permission)
     @classroom = nil
   end
 
@@ -15,5 +15,19 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def self.from_json(json)
+    Student.new(json['id'], json['age'], json['name'], parent_permission: json['parent_permission'])
+  end
+
+  def self.to_json
+    obj = {
+      'id' => @id,
+      'age' => @age,
+      'name' => @name,
+      'parent_permission' => @parent_permission
+    }
+    JSON.generate(obj)
   end
 end
