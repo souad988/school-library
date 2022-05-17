09c6073ee5ad4d33bf16da923798320f9cdea3ec
Bavon101 <bavonfate@gmail.com>
2022-05-17 14:01:34 +0300

    Add serializers

--- rental.rb
+++ rental.rb
@@ -9,4 +9,22 @@ class Rental
     @person = person
     person.rentals.push(self) unless person.rentals.include?(self)
   end
+
+  def self.from_json(json)
+    person = if json['person'].key?('specialization')
+               Teacher.from_json(json['person'])
+             else
+               Student.from_json(json['person'])
+             end
+    Rental.new(json['date'], Book.from_json(json['book']), person)
+  end
+
+  def self.to_json
+    obj = {
+      'date' => @date,
+      'book' => @book.to_json,
+      'person' => @person.to_json
+    }
+    JSON.generate(obj)
+  end
 end
